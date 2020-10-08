e052f04d8bf88cb1dc3d38614733e24fae63edae
Rowaida <rowaida.m@ku.th>
2020-10-08 11:00:36 +0700

    commit
    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,17 @@ import { MenuComponent } from './menu/menu.component';
 
 import { MatListModule } from '@angular/material/list';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
@@ -22,9 +29,22 @@ import { MatListModule } from '@angular/material/list';
     MatToolbarModule,
     FlexLayoutModule,
 
-    MatListModule
+    MatListModule,
+
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
+
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
+
+
+
 export class AppModule { }
+
+
+
+
+  
